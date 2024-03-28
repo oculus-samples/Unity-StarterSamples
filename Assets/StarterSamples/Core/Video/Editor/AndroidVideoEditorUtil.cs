f1337c30465f3975fa2127530aab00021a3136cd
Trevor Dasch <trevor.dasch@meta.com>
2024-03-28 14:23:18 -0700

    Fix AndroidVideoEditorUtil to be path agnostic
    Summary: This editor script was using hardcoded SampleFramework paths that are no longer valid. Instead, we should find the plugins dynamically.
    
    Reviewed By: AlexThiv
    
    Differential Revision: D55372509
    
    fbshipit-source-id: ea8d8b998e15a183a1c99f6cac3c1cd107a6bf63

--- Assets/StarterSamples/Core/Video/Editor/AndroidVideoEditorUtil.cs
+++ Assets/StarterSamples/Core/Video/Editor/AndroidVideoEditorUtil.cs
@@ -21,84 +21,74 @@
 using System.Collections.Generic;
 using UnityEditor;
 using System.IO;
+using UnityEngine;
 
 public class AndroidVideoEditorUtil
 {
-    private static readonly string videoPlayerFileName =
-        "Assets/Oculus/SampleFramework/Core/Video/Plugins/Android/java/com/oculus/videoplayer/NativeVideoPlayer.java";
+    private static readonly string[] VideoPluginNames = {
+        "NativeVideoPlayer", "audio360"
+    };
+    private const string ExoPlayerPackageName = "com.google.android.exoplayer:exoplayer";
+    private const string ExoPlayerVersion = "2.18.2";
+    private const string AndroidXPropertyName = "android.useAndroidX";
+    private const string AndroidXPropertyValue = "true";
+    private const AndroidSdkVersions MinimumTargetAndroidSdkVersion = (AndroidSdkVersions)31;
 
-    private static readonly string audio360PluginPath =
-        "Assets/Oculus/SampleFramework/Core/Video/Plugins/Android/Audio360/audio360.aar";
+    private static void SetPluginCompatibility(bool enabled)
+    {
+        string[] searchFolders = { OculusBuildSamples.GetSamplesRootPath() };
 
-    private static readonly string audio360Exo218PluginPath =
-        "Assets/Oculus/SampleFramework/Core/Video/Plugins/Android/Audio360/audio360-exo218.aar";
+        // Enable or Disable Android Video Plugins
+        foreach (var pluginName in VideoPluginNames)
+        {
+            foreach (var guid in AssetDatabase.FindAssets(pluginName, searchFolders))
+            {
+                string pluginPath = AssetDatabase.GUIDToAssetPath(guid);
+                PluginImporter pluginImporter = AssetImporter.GetAtPath(pluginPath) as PluginImporter;
+                if (pluginImporter != null)
+                {
+                    Debug.Log($"Setting Android Compatibility for {pluginPath} to {enabled}");
+                    pluginImporter.SetCompatibleWithPlatform(BuildTarget.Android, enabled);
+                    pluginImporter.SaveAndReimport();
+                }
+            }
+        }
+    }
 
     [MenuItem("Oculus/Samples/Video/Enable Native Android Video Player")]
     public static void EnableNativeVideoPlayer()
     {
-        // Enable native video player
-        PluginImporter nativeVideoPlayer = (PluginImporter)AssetImporter.GetAtPath(videoPlayerFileName);
-        if (nativeVideoPlayer != null)
-        {
-            nativeVideoPlayer.SetCompatibleWithPlatform(BuildTarget.Android, true);
-            nativeVideoPlayer.SaveAndReimport();
-        }
-
-        // Enable audio plugins
-        PluginImporter audio360 = (PluginImporter)AssetImporter.GetAtPath(audio360PluginPath);
-        PluginImporter audio360exo218 = (PluginImporter)AssetImporter.GetAtPath(audio360Exo218PluginPath);
-
-        if (audio360 != null && audio360exo218 != null)
-        {
-            audio360.SetCompatibleWithPlatform(BuildTarget.Android, true);
-            audio360exo218.SetCompatibleWithPlatform(BuildTarget.Android, true);
-            audio360.SaveAndReimport();
-            audio360exo218.SaveAndReimport();
-        }
+        // Enable video plugins
+        SetPluginCompatibility(true);
 
         // Enable gradle build with exoplayer
         Gradle.Configuration.UseGradle();
         var template = Gradle.Configuration.OpenTemplate();
-        template.AddDependency("com.google.android.exoplayer:exoplayer", "2.18.2");
+        template.AddDependency(ExoPlayerPackageName, ExoPlayerVersion);
         template.Save();
 
         var properties = Gradle.Configuration.OpenProperties();
-        properties.SetProperty("android.useAndroidX", "true");
+        properties.SetProperty(AndroidXPropertyName, AndroidXPropertyValue);
         properties.Save();
 
-        // Set the API level to 31
-        PlayerSettings.Android.targetSdkVersion = (AndroidSdkVersions)31;
+        // Set the target sdk version to the required minimum for the NativeVideoPlayer plugin
+        if (PlayerSettings.Android.targetSdkVersion < MinimumTargetAndroidSdkVersion)
+        {
+            PlayerSettings.Android.targetSdkVersion = MinimumTargetAndroidSdkVersion;
+        }
     }
 
     [MenuItem("Oculus/Samples/Video/Disable Native Android Video Player")]
     public static void DisableNativeVideoPlayer()
     {
-        // Disable native video player
-        PluginImporter nativeVideoPlayer = (PluginImporter)AssetImporter.GetAtPath(videoPlayerFileName);
-        if (nativeVideoPlayer != null)
-        {
-            nativeVideoPlayer.SetCompatibleWithPlatform(BuildTarget.Android, false);
-            nativeVideoPlayer.SaveAndReimport();
-        }
+        // Disable video plugins
+        SetPluginCompatibility(false);
 
-        // Disable audio plugins
-        PluginImporter audio360 = (PluginImporter)AssetImporter.GetAtPath(audio360PluginPath);
-        PluginImporter audio360exo218 = (PluginImporter)AssetImporter.GetAtPath(audio360Exo218PluginPath);
-
-        if (audio360 != null && audio360exo218 != null)
-        {
-            audio360.SetCompatibleWithPlatform(BuildTarget.Android, false);
-            audio360exo218.SetCompatibleWithPlatform(BuildTarget.Android, false);
-            audio360.SaveAndReimport();
-            audio360exo218.SaveAndReimport();
-        }
-
-        // remove exoplayer and sourcesets from gradle file (leave other parts since they are harmless).
+        // remove exoplayer from gradle file (leave other changes since they are potentially shared).
         if (Gradle.Configuration.IsUsingGradle())
         {
             var template = Gradle.Configuration.OpenTemplate();
-            template.RemoveDependency("com.google.android.exoplayer:exoplayer");
-            template.RemoveAndroidSetting("sourceSets.main.java.srcDir");
+            template.RemoveDependency(ExoPlayerPackageName);
             template.Save();
         }
     }
